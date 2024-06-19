29e2026aa54410db1e8e5a0e2e4e7bee1a036349
Nico Stern <s92nster@uni-bonn.de>
2024-06-19 10:05:41 +0200

    Initial Commit

---
+++ checkmathe-ios/checkmathe_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkmathe_iosApp.swift
+//  checkmathe-ios
+//
+//  Created by Nico Stern on 19.06.24.
+//
+
+import SwiftUI
+
+@main
+struct checkmathe_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
